877861c9758794fa38569f03ba2d062d75d70b76
Salman Khan <salmantechhub336@gmail.com>
2026-06-08 20:02:17 +0500

    fetched photos api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {fetchPhotos} from './api/mediaApi'
 
 const App = () => {
+
+
+  // function getPhotos(){
+  //   fetchPhotos()
+  // }
   return (
     <div className='h-screen w-full bg-gray-950 text-white'> 
+    <button onClick = {async()=>{
+      const data = await fetchPhotos('nature')
+      console.log(data.results);
+    }}>
+      get photos
+    </button>
     
     </div>
   )
