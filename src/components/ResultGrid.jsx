e22cd82a3148eb01d3a74d4055bf1fb579d8d169
Salman Khan <salmantechhub336@gmail.com>
2026-06-11 04:06:23 +0500

    changes

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import {fetchPhotos, fetchVideos} from '../api/mediaApi'
 import {setQuery, setLoading, setError, setResults} from '../redux/features/searchSlice'
 import {useDispatch, useSelector} from 'react-redux'
+import { useEffect } from 'react';
 
 const ResultGrid = () => {
+
+  const {query, activeTab, results, loading, error } = useSelector((store)=>store.search.results);
+  useEffect(function(){
+
+    
+
+  },[])
 
  
   return (
