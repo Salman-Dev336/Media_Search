186b9cbca2f959533d50fa8d9755dd763e22f30f
Salman Khan <salmantechhub336@gmail.com>
2026-06-10 21:27:21 +0500

    making result grid

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import {fetchPhotos, fetchVideos} from '../api/mediaApi'
+import {setQuery, setLoading, setError, setResults} from '../redux/features/searchSlice'
+import {useDispatch, useSelector} from 'react-redux'
 
 const ResultGrid = () => {
+
+ 
   return (
+
     <div>ResultGrid</div>
   )
 }
