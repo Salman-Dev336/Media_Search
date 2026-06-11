2f72574d505152821c7524588ce45047db5c0b42
Salman Khan <salmantechhub336@gmail.com>
2026-06-11 05:47:12 +0500

    fetch photos

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -6,17 +6,23 @@ import { useEffect } from 'react';
 
 const ResultGrid = () => {
 
-  const {query, activeTab, results, loading, error } = useSelector((store)=>store.search.results);
-  useEffect(function(){
+  const {query, activeTab, results, loading, error } = useSelector((store)=>store.search);
+  
 
-    
-
-  },[])
-
- 
+  const getData = async ()=>{
+    if(activeTab == 'photos'){
+      const data = await fetchPhotos(query);
+      
+      console.log(data);
+    }
+  }
   return (
 
-    <div>ResultGrid</div>
+    <div>
+      <button onClick={getData}>
+        Get Data
+      </button>
+    </div>
   )
 }
 
