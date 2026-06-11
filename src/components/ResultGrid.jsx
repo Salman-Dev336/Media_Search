34f4842b6af28c4647c754a9370fd0782a8ef0c3
Salman Khan <salmantechhub336@gmail.com>
2026-06-11 06:41:35 +0500

    changes

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -10,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 
 const ResultGrid = () => {
+  const dispatch = useDispatch();
   const { query, activeTab, results, loading, error } = useSelector(
     (store) => store.search,
   );
@@ -17,34 +18,49 @@ const ResultGrid = () => {
   useEffect(
     function () {
       const getData = async () => {
-        let data;
-        if (activeTab == "photos") {
-          let response = await fetchPhotos(query);
-          data = response.results.map((items) => ({
-            id: items.id,
-            type: "photo",
-            title: items.alt_description,
-            thumbnail: items.urls.small,
-            src: items.urls.full,
-          }));
+        try {
+          dispatch(setLoading());
+          let data;
+          if (activeTab == "photos") {
+            let response = await fetchPhotos(query);
+            data = response.results.map((items) => ({
+              id: items.id,
+              type: "photo",
+              title: items.alt_description,
+              thumbnail: items.urls.small,
+              src: items.urls.full,
+            }));
+          }
+          if (activeTab == "videos") {
+            let response = await fetchVideos(query);
+            data = response.videos.map((items) => ({
+              id: items.id,
+              type: "video",
+              title: items.user.name,
+              thumbnail: items.image,
+              src: items.video_files[0].link,
+            }));
+          }
+          // console.log(data);
+          dispatch(setResults(data));
+        } catch (err) {
+          dispatch(setError(err));
         }
-        if (activeTab == "videos") {
-          let response = await fetchVideos(query);
-          data = response.videos.map((items) => ({
-            id: items.id,
-            type: "video",
-            title: items.user.name,
-            thumbnail: items.image,
-            src: items.video_files[0].link,
-          }));
-        }
-        console.log(data);
       };
       getData();
     },
     [query, activeTab],
   );
-  return <div></div>;
+
+  if (error) return <h1>Error </h1>;
+  if (loading) return <h1>loading...</h1>;
+  return (
+    <div>
+      {results.map((items, index) => {
+        return <h1>{items.title}</h1>
+      })}
+    </div>
+  );
 };
 
 export default ResultGrid;
