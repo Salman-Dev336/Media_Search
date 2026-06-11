848fcee26107d0b8bc2c19cf242dea5b4742f7a3
Salman Khan <salmantechhub336@gmail.com>
2026-06-11 06:25:38 +0500

    changes

--- src/components/ResultGrid.jsx
+++ src/components/ResultGrid.jsx
@@ -20,11 +20,23 @@ const ResultGrid = () => {
         let data;
         if (activeTab == "photos") {
           let response = await fetchPhotos(query);
-          data = response.results;
+          data = response.results.map((items) => ({
+            id: items.id,
+            type: "photo",
+            title: items.alt_description,
+            thumbnail: items.urls.small,
+            src: items.urls.full,
+          }));
         }
         if (activeTab == "videos") {
           let response = await fetchVideos(query);
-          data = response.videos;
+          data = response.videos.map((items) => ({
+            id: items.id,
+            type: "video",
+            title: items.user.name,
+            thumbnail: items.image,
+            src: items.video_files[0].link,
+          }));
         }
         console.log(data);
       };
